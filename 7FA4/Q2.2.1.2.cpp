d25d942976741867cf4db6091d35ce2f4ef25104
WzhDnwzWzh <wzh20060818wzh@outlook.com>
2021-08-06 09:14:04 +0800

    add 7fa4 q2.2.1.2

--- 7FA4/Q2.2.1.2.cpp
+++ 7FA4/Q2.2.1.2.cpp
@@ -12,29 +12,37 @@ string str[20], last = "";
 char dragon[401];
 bool mark[20];
 
-void dfs(int n) {
+void dfs(int n)
+{
     int tmp;
     string tmp2;
 
     if (n == 0)
-        if (ans < len) {
+        if (ans < len)
+        {
             ans = len;
             return;
         }
     int begin;
-    for (int i = 0; i < num; i++) {
-        if (!mark[i] && str[i] != last) {
-            for (int j = (len > str[i].length() ? len - str[i].length() : 0); j < len; j++) {
+    for (int i = 0; i < num; i++)
+    {
+        if (!mark[i] && str[i] != last)
+        {
+            for (int j = (len > str[i].length() ? len - str[i].length() : 0); j < len; j++)
+            {
                 begin = j;
-                for (int k = j; k < len; k++) {
-                    if (dragon[k] != str[i][k - j]) {
+                for (int k = j; k < len; k++)
+                {
+                    if (dragon[k] != str[i][k - j])
+                    {
                         begin = -1;
                         break;
                     }
                 }
-                if (begin != -1 &&
-                    (len == 1 || begin != (len > str[i].length() ? len - str[i].length() : 0))) {
-                    for (int j = begin; j < begin + str[i].length(); j++) dragon[j] = str[i][j - begin];
+                if (begin != -1 && (len == 1 || begin != (len > str[i].length() ? len - str[i].length() : 0)))
+                {
+                    for (int j = begin; j < begin + str[i].length(); j++)
+                        dragon[j] = str[i][j - begin];
                     tmp = len;
                     tmp2 = last;
                     len = begin + str[i].length();
@@ -54,16 +62,20 @@ void dfs(int n) {
     }
 }
 
-int main() {
+int main()
+{
     int n, a[10];
     string ts;
     memset(mark, false, sizeof(mark));
 
     cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+    {
         cin >> ts;
-        for (int j = 0; j < a[i]; j++) str[num++] = ts;
+        for (int j = 0; j < a[i]; j++)
+            str[num++] = ts;
     }
     cin >> dragon[0];
 
