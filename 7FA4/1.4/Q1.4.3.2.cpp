d2fbca3499acacd110f10e7b3b32cc89ab3674c8
WzhDnwzWzh <wzh20060818wzh@outlook.com>
2021-08-16 20:53:03 +0800

    add 7fa4 q1.4.3.2

--- 7FA4/1.4/Q1.4.3.2.cpp
+++ 7FA4/1.4/Q1.4.3.2.cpp
@@ -7,19 +7,41 @@ using namespace std;
 
 int main()
 {
-    long long n, w, minn = 1e10, pos = -1;
+    long long n, w[400001], sum;
+    bool is_ans;
 
     cin >> n;
-    for (long long i = 0; i < n; i++)
+    cin >> w[1];
+    w[1]--;
+    w[n + 1] = w[1];
+    for (int i = 2; i <= n; i++)
     {
-        cin >> w;
-        if (w < minn)
+        cin >> w[i];
+        w[i]--;
+        w[n + i] = w[i];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        is_ans = true;
+        sum = 0;
+        for (int j = i; j <= i + n; j++)
         {
-            minn = w;
-            pos = i + 1;
+            sum += w[j];
+            if (sum >= 0)
+            {
+                is_ans = false;
+                break;
+            }
+        }
+
+        if (is_ans)
+        {
+            cout << i << endl;
+            return 0;
         }
     }
 
-    cout << pos << endl;
+    cout << "-1" << endl;
     return 0;
 }
