76e5670425cb20515cdbfbe09cdeba97d9343fe3
WzhDnwzWzh <wzh20060818wzh@outlook.com>
2021-08-22 16:55:34 +0800

    add p3952

--- Luogu/P3952.cpp
+++ Luogu/P3952.cpp
@@ -4,62 +4,50 @@
 #include <iostream>
 #include <cstring>
 
-using namespace std;
-
-int main()
-{
+int main() {
     int t, l, o, nx, ny, unshown;
-    string tmp, stack[100], x, y;
+    std::string tmp, stack[100], x, y;
     int depth, top, maxn;
     bool err, f[100];
     memset(f, 0, sizeof(f));
 
-    cin >> t;
-    while (t > 0)
-    {
+    std::cin >> t;
+    while (t > 0) {
         top = depth = maxn = 0;
         unshown = 100;
         err = false;
 
-        cin >> l >> tmp;
+        std::cin >> l >> tmp;
         if (tmp[2] == '1')
             o = 0;
-        else
-        {
+        else {
             o = 0;
             for (int each = 4; tmp[each] >= '0' && tmp[each] <= '9'; each++)
                 o = o * 10 + tmp[each] - '0';
         }
-        for (int each = 0; each < l; each++)
-        {
-            cin >> tmp;
-            if (tmp == "F")
-            {
-                cin >> stack[top] >> x >> y;
+        for (int each = 0; each < l; each++) {
+            std::cin >> tmp;
+            if (tmp == "F") {
+                std::cin >> stack[top] >> x >> y;
 
                 for (int j = 0; j < top; j++)
                     if(stack[j] == stack[top])
                         err = true;
                 if (x == "n" && y == "n")
                     f[top] = false;
-                if (x == "n" && y != "n")
-                {
+                if (x == "n" && y != "n") {
                     f[top] = false;
                     if (top < unshown)
                         unshown = top;
                 }
-                if (x != "n" && y == "n")
-                {
-                    if (top < unshown)
-                    {
+                if (x != "n" && y == "n") {
+                    if (top < unshown) {
                         f[top] = true;
                         depth++;
-                    }
-                    else
+                    } else
                         f[top] = false;
                 }
-                if (x != "n" && y != "n")
-                {
+                if (x != "n" && y != "n") {
                     nx = ny = 0;
                     for (int j = 0; j < x.length(); j++)
                         nx = nx * 10 + x[j] - '0';
@@ -72,13 +60,10 @@ int main()
                 top++;
                 if (depth > maxn)
                     maxn = depth;
-            }
-            else
-            {
+            } else {
                 if (top == 0)
                     err = true;
-                else
-                {
+                else {
                     if (top - 1 == unshown)
                         unshown = 100;
                     else if (f[top - 1])
@@ -89,11 +74,11 @@ int main()
         }
 
         if (err || top != 0)
-            cout << "ERR" << endl;
+            std::cout << "ERR" << std::endl;
         else if (maxn == o)
-            cout << "Yes" << endl;
+            std::cout << "Yes" << std::endl;
         else
-            cout << "No" << endl;
+            std::cout << "No" << std::endl;
         t--;
     }
 
