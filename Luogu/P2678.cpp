db7668cf7b3b86c687d35b5518a849bbface99b8
WzhDnwzWzh <wzh20060818wzh@outlook.com>
2021-08-23 16:04:01 +0800

    add p2678

---
+++ Luogu/P2678.cpp
@@ -0,0 +1,36 @@
+// P2678 [NOIP2015 提高组] 跳石头
+// WzhDnwzWzh
+
+#include <iostream>
+
+int l, n, m;
+int d[50000];
+
+int check(int mid) {
+    int ans = 0, pos = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (d[i] - pos < mid) ans++;
+        else pos = d[i];
+    }
+
+    return ans <= m;
+}
+
+int main() {
+    std::cin >> l >> n >> m;
+    for (int i = 0; i < n; i++) {
+        std::cin >> d[i];
+    }
+
+    int left = 0, right = l, mid;
+    while (left < right) {
+        mid = (left + right) >> 1;
+        if (check(mid)) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    if (!check(left)) left--;
+    std::cout << left << std::endl;
+    return 0;
+}
