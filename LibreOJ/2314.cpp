984c1078e92c88c58a62675cb3648d6221089064
WzhDnwzWzh <wzh20060818wzh@outlook.com>
2021-10-12 16:40:27 +0800

    add loj 2314

---
+++ LibreOJ/2314.cpp
@@ -0,0 +1,11 @@
+// #2314. 「NOIP2017」小凯的疑惑
+// WzhDnwzWzh
+
+#include <iostream>
+
+int main() {
+    long long a, b;
+    std::cin >> a >> b;
+    std::cout << a * b - a - b << std::endl;
+    return 0;
+}
