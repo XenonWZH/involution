b9fef89c200ce0655b91a39810ddbd31c7607944
WzhDnwzWzh <wzh20060818wzh@outlook.com>
2021-10-16 14:02:00 +0800

    add loj 3380

--- LibreOJ/3380.cpp
+++ LibreOJ/3380.cpp
@@ -1,6 +1,7 @@
-// P7076 [CSP-S2020] 动物园
+// #3380. 「CSP-S 2020」动物园
 // WzhDnwzWzh
 
+#include <cstdio>
 #include <cstring>
 #include <iostream>
 #include <set>
@@ -8,6 +9,8 @@
 typedef unsigned long long ull;
 
 int main() {
+    freopen("zoo.in", "r", stdin);
+    freopen("zoo.out", "w", stdout);
     ull n, m, c, k;
     static ull animal[1000005];
 
@@ -36,5 +39,7 @@ int main() {
     if (k == 64 && n == 0)
         std::cout << "18446744073709551616" << std::endl;
     else std::cout << ans << std::endl;
+    fclose(stdin);
+    fclose(stdout);
     return 0;
 }
