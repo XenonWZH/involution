89d421969c7cb37053dbe28d148f3146bd05da84
XenonWZH <xenonwzh@gmail.com>
2022-02-24 16:09:20 +0800

    add acwing 274

---
+++ AcWing/274.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+const int MAXN = 1000, MAXL = 200, INF = 0x3f3f3f3f;
+
+int main() {
+    int l, n;
+    static int c[MAXL + 1][MAXL + 1], p[MAXN + 1];
+
+    std::cin >> l >> n;
+    for (int i = 1; i <= l; i++) {
+        for (int j = 1; j <= l; j++) {
+            scanf("%d", &c[i][j]);
+        }
+    }
+    for (int i = 1; i <= n; i++) scanf("%d", &p[i]);
+
+    static int f[MAXN + 1][MAXL + 1][MAXL + 1];
+    memset(f, 0x3f, sizeof(f));
+    p[0] = 3;
+    f[0][1][2] = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int x = 1; x <= l; x++) {
+            for (int y = 1; y <= l; y++) {
+                if (x == y || x == p[i] || y == p[i]) continue;
+                f[i + 1][x][y] = std::min(f[i + 1][x][y], f[i][x][y] + c[p[i]][p[i + 1]]);
+                f[i + 1][x][p[i]] = std::min(f[i + 1][x][p[i]], f[i][x][y] + c[y][p[i + 1]]);
+                f[i + 1][p[i]][y] = std::min(f[i + 1][p[i]][y], f[i][x][y] + c[x][p[i + 1]]);
+            }
+        }
+    }
+
+    int ans = INF;
+    for (int i = 1; i <= l; i++) {
+        for (int j = 1; j <= l; j++) {
+            ans = std::min(ans, f[n][i][j]);
+        }
+    }
+
+    std::cout << ans << std::endl;
+
+    return 0;
+}
