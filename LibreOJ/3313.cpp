d754f59250844e6c7efa544355ef4c5ea00aab92
XenonWZH <xenonwzh@gmail.com>
2022-07-18 09:02:21 +0800

    add loj 3313

--- LibreOJ/3313.cpp
+++ LibreOJ/3313.cpp
@@ -17,27 +17,41 @@ int main() {
             scanf("%lld", &a[i]);
         }
 
-        long long ans = 0, line = 0, dance1 = 0, dance2 = 0;
+        long long line = 0, dance1 = 0, dance2 = 0, ans = 0, nxt = 0;
 
         for (int i = 1; i <= n; i++) {
-            long long after = 0, tmp = line + dance1 - a[i + 1];
-
-            if (tmp > 0) {
-                if (tmp > line) dance1 = dance1 + line - tmp, tmp = line;
-                if (tmp > dance1) line = line + dance1 - tmp, tmp = dance1;
-
-                line -= tmp, dance1 -= tmp, a[i + 1] -= tmp, after = tmp;
+            if (a[i + 1] < dance1 + line) {
+                int delta = dance1 + line - a[i + 1];
+                if (line > a[i + 1]) {
+                    line = a[i + 1];
+                    delta = dance1;
+                }
+                if (dance1 > a[i + 1]) {
+                    dance1 = a[i + 1];
+                    delta = line;
+                }
+                line -= delta;
+                dance1 -= delta;
+                nxt = delta;
+                a[i + 1] -= nxt;
             }
 
             a[i + 1] -= line + dance1;
             ans += a[i];
 
-            long long before = std::min(a[i], a[i + 1]);
-            line += before;
-            a[i] -= before, a[i + 1] -= before;
+            if (a[i] <= a[i + 1]) {
+                line += a[i];
+                a[i + 1] -= a[i];
+                a[i] = 0;
+            } else {
+                line += a[i + 1];
+                dance2 += a[i] - a[i + 1];
+                a[i] = a[i + 1] = 0;
+            }
 
-            dance2 += a[i];
-            a[i + 1] += after, ans -= after;
+            a[i + 1] += nxt;
+            ans -= nxt;
+            nxt = 0;
 
             std::swap(dance1, dance2);
         }
