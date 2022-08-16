8de40b34441dee5f9a54e07da61e431b3e48868a
XenonWZH <xenonwzh@gmail.com>
2022-08-16 13:57:39 +0800

    qwq

--- LibreOJ/537.cpp
+++ LibreOJ/537.cpp
@@ -1,22 +1,26 @@
-// #537. 「LibreOJ NOIP Round #1」DNA 序列
-// WzhDnwzWzh
-
 #include <iostream>
+#include <string>
 #include <unordered_map>
 
 int main() {
-    static std::string dna;
+    std::string dna;
     int k;
 
     std::cin >> dna >> k;
 
-    static std::unordered_map<std::string, int> cnt;
-    for (int i = 0, len = dna.size(); i <= len - k; i++) ++cnt[dna.substr(i, k)];
+    int len = dna.length();
+    static std::unordered_map<std::string, int> hash;
+
+    for (int i = 0; i <= len - k; i++) {
+        hash[dna.substr(i, k)]++;
+    }
 
     int ans = 0;
-    for (auto each : cnt) if (each.second > ans) ans = each.second;
+    for (auto each : hash) {
+        ans = std::max(ans, each.second);
+    }
 
-    std::cout << ans << std::endl;
+    printf("%d\n", ans);
 
     return 0;
 }
