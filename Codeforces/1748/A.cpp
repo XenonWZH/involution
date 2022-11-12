3490b46715ec0342d21ec144d39e99bb379ca077
XenonWZH <xenonwzh@gmail.com>
2022-11-13 00:49:46 +0800

    qwq

---
+++ Codeforces/1748/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+namespace XenonWZH {
+    void solve() {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", (n + 1) / 2);
+    }
+
+    int main() {
+        int t;
+
+        scanf("%d", &t);
+
+        while (t--) solve();
+
+        return 0;
+    }
+}
+
+int main() {
+    return XenonWZH::main();
+}
